a1cfa160f5aed5d2e082399c41e76ec33a460c1d
Sneha Choudhary <97403777+choudharysneha1708@users.noreply.github.com>
2023-06-12 14:12:44 +0530

    Added solution - LeetHub

---
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+    // private: 
+    // void change(vector<int>&dis){
+    //     for(int i=0;i<dis.size();++i){
+    //         dis[i]=-1;
+    //     }
+    // }
+  public:
+    /*  Function to implement Bellman Ford
+    *   edges: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+        // Code here
+        vector<int> dis(V, 1e8);
+        dis[S]=0;
+        for(int i=0;i<V-1;++i){
+            for(auto it: edges){
+                int x=it[0];
+                int y=it[1];
+                int edgeLen=it[2];
+                if(dis[y]>dis[x]+edgeLen){
+                    dis[y]=dis[x]+edgeLen;
+                }
+            }
+        }
+        for(auto it: edges){
+            int x=it[0];
+            int y=it[1];
+            int edgeLen=it[2];
+            if(dis[y]!=1e8 && dis[y]>dis[x]+edgeLen){
+             return{-1};
+            }
+        }
+        return dis;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, m;
+        cin >> N >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src;
+        cin >> src;
+
+        Solution obj;
+        vector<int> res = obj.bellman_ford(N, edges, src);
+
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
